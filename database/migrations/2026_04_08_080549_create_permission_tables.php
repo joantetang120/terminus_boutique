21625355f78a8af5067d5a3bc8dda50b59ce9503
Jason30-cm <joeby6779@gmail.com>
2026-04-11 15:32:03 +0100

    fix:table_permission

--- database/migrations/2026_04_08_080549_create_permission_tables.php
+++ database/migrations/2026_04_08_080549_create_permission_tables.php
@@ -24,12 +24,13 @@ return new class extends Migration
          * See `docs/prerequisites.md` for suggested lengths on 'name' and 'guard_name' if "1071 Specified key was too long" errors are encountered.
          */
         Schema::create($tableNames['permissions'], static function (Blueprint $table) {
+
             $table->id(); // permission id
-            $table->string('name');
-            $table->string('guard_name');
+            $table->string('name', 125);
+            $table->string('guard_name', 125);
             $table->timestamps();
 
-            $table->unique(['name', 'guard_name']);
+            // $table->unique(['name', 'guard_name']);
         });
 
         /**
@@ -41,8 +42,8 @@ return new class extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key'])->nullable();
                 $table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
             }
-            $table->string('name');
-            $table->string('guard_name');
+            $table->string('name', 125);
+            $table->string('guard_name', 125);
             $table->timestamps();
             if ($teams || config('permission.testing')) {
                 $table->unique([$columnNames['team_foreign_key'], 'name', 'guard_name']);
