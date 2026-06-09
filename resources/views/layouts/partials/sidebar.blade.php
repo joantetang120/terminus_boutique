789b6f7c7191f6349a45aa03ce05ad7b5ee9c240
joantetang120 <joantetang120@gmail.com>
2026-06-09 13:49:11 +0100

    feat: ajout du calcul des marges

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -72,6 +72,13 @@
                 <span x-show="expanded" x-cloak>Comptabilité</span>
             </a>
         </li>
+        <li>
+            <a href="{{ route('marge.index') }}" class="{{ request()->routeIs('marge.*') ? 'active' : '' }}"
+               :title="!expanded ? 'Marges' : ''">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" /></svg>
+                <span x-show="expanded" x-cloak>Marges</span>
+            </a>
+        </li>
         @endcan
         @can('facture.view')
         <li class="nav-item {{ request()->routeIs('comptabilite.factures*') ? 'active' : '' }}">
